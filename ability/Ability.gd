2561bec41f5d41ef82f8fe87d90f194b93f72426
frontrider <frontrider@tutanota.com>
2024-12-28 13:48:35 +0100

    document the ability resource

--- ability/Ability.gd
+++ ability/Ability.gd
@@ -1,14 +1,22 @@
 class_name AbilityContainer
 extends Resource
 
+#Identifier that that should also be passed to Neuro
 @export
 var name:StringName
 
+#description that that should also be passed to Neuro
+@export
+var description:StringName =""
+
+#Text used when the action shows on the UI
 @export
 var tooltip:StringName
 
+# displays the action in-game
 @export
 var icon:PackedScene
 
+#spawned into the scene on use.
 @export
 var on_use:PackedScene
