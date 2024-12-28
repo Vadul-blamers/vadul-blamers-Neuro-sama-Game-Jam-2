4a099cd9a6608a443a04e21fab0b849c0eb28c35
That one guy <96492071+ThatOneGuy-mp4@users.noreply.github.com>
2024-12-28 14:11:25 -0600

    I keep forgetting to change this back
    Yeah you have to switch the scene to greggs map to play on the map but I forgot to put it back afterwards.

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -11,15 +11,15 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/greggs_map.tscn")
+	get_tree().change_scene_to_file("res://game/play.tscn")
 
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+	get_tree().change_scene_to_file("res://menu/settings_menu.tscn")
 
 
 func _on_credits_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	get_tree().change_scene_to_file("res://menu/credits.tscn")
 
 
 func _on_exit_button_pressed() -> void:
