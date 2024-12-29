d8365b3d94c105f22af1d33f4d85ab45e57dd796
That one guy <96492071+ThatOneGuy-mp4@users.noreply.github.com>
2024-12-29 15:27:58 -0600

    Updated Greggs Map Again
    Greggs map is redone with the 2x scaled tiles. The map still doesn't have collision. Who needs it, really.

--- GameState.gd
+++ GameState.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var player: Node2D
+
+static var current_room: Room
+
+static func set_current_room(room: Room) -> void:
+	current_room = room
+
+static func get_current_room() -> Room:
+	return current_room
--- game/map.gd
+++ game/map.gd
@@ -11,11 +11,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-#move the ui along with the camera position changes
-#it's not ideal. but the ui isn't my job so. you can just delete this if it doesn't work for the ui.
-#also, it's not actually moving the ui LULE. i don't know how to implement the ui so this is just a placeholder image.
-func move_ui() -> void:
-	$Placeholder.global_position.x = get_viewport().get_camera_2d().global_position.x
-	$Placeholder.global_position.y = get_viewport().get_camera_2d().global_position.y - get_viewport().get_camera_2d().get_viewport_rect().size.y / 2 + 192 / 2
-	pass
